f5de2b739e326aea23e6b39c513c80774d016a59
Zolisa001 <zolisawelani@gmail.com>
2013-03-23 14:34:41 +0200

    Adding GuessGame

---
+++ GuessGame.java
@@ -0,0 +1,34 @@
+
+// Programmer name: Zolisa Welani
+
+public class GuessGame{
+
+	public static void main(String[] args){
+
+	}
+	
+	public void input(){
+		// Onke name the secret number with a variable secret;
+		int lowerbound, upperbound;
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Guess the number between 1 and 500");
+		int attempt = new input.nextInt();
+
+		if (attempt == secret){
+			System.out.println("You very Good buddy! Your guess is correct.");
+		}
+		else{
+			if (attempt < secret){
+				System.out.println("The number you have entered is lower than the secret number");		
+				lowerbound  = attempt;			
+			}
+			if (attempt > secret){
+				System.out.println("The number you have entered is higher than the secret number");
+				upperbound = attempt;
+			}
+			
+		}
+
+	}
+}
